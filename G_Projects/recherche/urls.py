504bca3914c607895c7589ca6dfcca9ef137e202
Yonohi <loic.planat2@gmail.com>
2022-07-01 15:22:11 +0200

    maj recherche

--- G_Projects/recherche/urls.py
+++ G_Projects/recherche/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'recherche'
 urlpatterns = [
     path("recherche/", views.home_recherche, name="home_recherche"),
+    path("recherche/resultats/<str:query>/", views.resultats_recherche, name="resultats"),
 ]
--- G_Projects/recherche/views.py
+++ G_Projects/recherche/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def home_recherche(request):
+    if request.method == "POST":
+        query = request.POST.get('search_input')
+        return redirect('recherche:resultats', query)
     context = {}
     return render(request, template_name='recherche/home_recherche.html', context=context)
+
+def resultats_recherche(request, query):
+    if request.method == "POST":
+        new_query = request.POST.get('search_input')
+        return redirect('recherche:resultats', new_query)
+    context = {'query': query}
+    return render(request, template_name='recherche/resultats.html', context=context)
